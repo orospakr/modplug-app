8c815325164fcd22b140f8673b5daa8f5d3d56a2
Andrew Clunis <andrew@orospakr.ca>
2019-11-29 22:45:40 -0500

    Initial Commit

---
+++ ModPlug/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ModPlug
+//
+//  Created by Andrew Clunis on 2019-11-29.
+//  Copyright © 2019 Andrew Clunis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
